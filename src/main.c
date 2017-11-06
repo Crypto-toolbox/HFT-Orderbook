c3d0fe9a9fd99b63c07c742bef7a9b8d3f898132
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-06 10:35:58 +0100

    replaced pythonic string comparison with C comparison

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include "CuTest.h"
 #include "hftlob.h"
 
 int main(int argc, char* argv[]){
     int i;
+    printf("Running main..\n");
     for(i=0; i<argc; ++i){
-        if (argv[i] == "--test"){
+        if (strcmp(argv[i], "--test") == 0){
             printf("--test flag passed, running cuTest TestSuite..", i, argv[i]);
             RunAllTests();
         }
