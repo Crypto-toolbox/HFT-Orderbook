d2b8c23805898551ac004ac77ede7bec554b9281
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-07 12:54:50 +0100

    add build folder

---
+++ build/CMakeFiles/HFT_Orderbook.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/nils/git/HFT-Orderbook/src/CuTest.c" "/home/nils/git/HFT-Orderbook/build/CMakeFiles/HFT_Orderbook.dir/src/CuTest.c.o"
+  "/home/nils/git/HFT-Orderbook/src/datastructs.c" "/home/nils/git/HFT-Orderbook/build/CMakeFiles/HFT_Orderbook.dir/src/datastructs.c.o"
+  "/home/nils/git/HFT-Orderbook/src/funcs.c" "/home/nils/git/HFT-Orderbook/build/CMakeFiles/HFT_Orderbook.dir/src/funcs.c.o"
+  "/home/nils/git/HFT-Orderbook/src/main.c" "/home/nils/git/HFT-Orderbook/build/CMakeFiles/HFT_Orderbook.dir/src/main.c.o"
+  "/home/nils/git/HFT-Orderbook/src/testCases.c" "/home/nils/git/HFT-Orderbook/build/CMakeFiles/HFT_Orderbook.dir/src/testCases.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
