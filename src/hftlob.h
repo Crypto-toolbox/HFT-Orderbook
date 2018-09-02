e8790767bca9717d58eab2f007223b72851dbe46
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-09-02 20:24:59 +0200

    Update header file with docstrings.
    Also add definitions for new functions,
    and sort them in a more logical way.
    
    Future commits should continuously move
    any documentation existing in the .c files
    to the hftlob.h file.

--- src/hftlob.h
+++ src/hftlob.h
@@ -1,7 +1,43 @@
 #ifndef HFTLOB_H_
 #define HFTLOB_H_
+
 /**
- * CUSTOM STRUCTS
+ * Constants
+ */
+extern const uuid_t BINUUID;
+
+/**
+ * HFT Custom Struct Type definitions and functions.
+ *
+ * This sections includes initializer, constructor and desctructor functions
+ * for the defined structs. Other functions using these structs can be found
+ * further below, in the relevant API sections of this file.
+ *
+ *
+ * Initializer functions set the struct's values at the given pointer to 0
+ * - or equivalent - * on all fields, explicitly. This will override any
+ * previously set values.
+ *
+ * Constructor functions (those starting with "new*"), create a pointer for
+ * the relevant struct, allocate memory using malloc(), and return it.
+ *
+ * Destructor functions (those starting with "delete*"), free any memory
+ * allocated by the constructor methods.
+ *
+ * .. Note::
+ *
+ *        We only free() resources that were explicitly malloc'd by us. Any
+ *        other memory allocation must be free'd by you beforehand, otherwise
+ *        this will cause a memory leak.
+ *
+ * .. Note::
+ *
+ *      While destructor functions assert that the struct's references to other
+ *      structs have been NULL'd, they do NOT check other structs for references
+ *      to the struct that is being deleted.
+ *
+ *      We assume it has been unlinked from the remaining structs properly,
+ *      if its struct references are NULL!
  */
 
 typedef struct Order{
@@ -17,6 +53,16 @@ typedef struct Order{
     int exchangeId;
 } Order;
 
+void
+initOrder(Order *order);
+
+Order*
+newOrder(void);
+
+void
+deleteOrder(Order *ptr_order);
+
+
 typedef struct Limit{
     double limitPrice;
     double size;
@@ -29,14 +75,39 @@ typedef struct Limit{
     struct Order *tailOrder;
 } Limit;
 
+void
+initLimit(Limit *limit);
+
+Limit*
+newLimit(void);
+
+void
+deleteLimit(Limit *ptr_limit);
+
+Limit*
+newRoot(void);
+
+void
+deleteRoot(Limit *ptr_limit);
+
+
 typedef struct Book{
     Limit *bids;
     Limit *asks;
 } Book;
 
+void
+initBook(Book *book);
+
+Book*
+newBook(void);
+
+void
+deleteBook(Book *ptr_book);
+
 
 /**
- * Book API Functions
+ * Public API Functions for the HFT-Orderbook library.
  */
 
 void
@@ -46,18 +117,7 @@ int
 removeOrderFromBook(Book* ptr_book, Order* ptr_order);
 
 
-/**
- * INIT FUNCTIONS
- */
 
-void
-initBook(Book *book);
-
-void
-initOrder(Order *order);
-
-void
-initLimit(Limit *limit);
 
 
 /**
@@ -76,9 +136,6 @@ removeOrder(Order *order);
 /**
  * BINARY SEARCH TREE FUNCTIONS
  */
-
-Limit*
-createRoot(void);
 
 Limit*
 addLimit(Limit *root, Limit *limit);
