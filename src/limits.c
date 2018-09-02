2d5d685624a82bac64f400de3c44e52efca95178
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-09-02 19:20:07 +0200

    Merge bst.c into limits.c.

--- src/limits.c
+++ src/limits.c
@@ -12,26 +12,70 @@
  *
  */
 
- #include <math.h>
+#include <math.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "hftlob.h"
 
 
-/*
- * Limit Struct API
+/**
+ * API for Limit struct-based Binary Search Tree Implementation.
  */
 
 Limit*
-createRoot(void){
+newRoot(void){
     /**
-     * Create a Limit structure as root and return a ptr to it.
+     * Create a Limit struct as root and return its pointer.
      */
     Limit *ptr_limit = malloc(sizeof(Limit));
     initLimit(ptr_limit);
     ptr_limit->limitPrice = -INFINITY;
+    assert(isRoot(ptr_limit));
     return ptr_limit;
+}
+
+void
+deleteRoot(Limit *ptr_root){
+    /**
+     * Delete the Limit struct acting as a BST root at the given pointer.
+     *
+     * We will traverse
+     */
+    assert(isRoot(ptr_root));
+    Limit *ptr_foundLimit = getMinimumLimit(ptr_root);
+    while (ptr_foundLimit != ptr_root){
+        removeLimit(ptr_foundLimit);
+        deleteLimit(ptr_foundLimit);
+        ptr_foundLimit = getMinimumLimit(ptr_root);
+    }
+    deleteLimit(ptr_root);
+}
+
+Limit*
+newLimit(double value){
+    /**
+     * Create a new Limit struct and return a pointer to it.
+     */
+    Limit *ptr_limit = malloc(sizeof(Limit));
+    ptr_limit->limitPrice = value;
+    return ptr_limit;
+}
+
+void
+deleteLimit(Limit *ptr_limit){
+    /**
+     * Delete the Limit struct at the given pointer.
+     *
+     * This first asserts that the limit has no children left, before deleting
+     * any orders that are still stored in the Limit struct.
+     *
+     */
+    assert(ptr_limit->leftChild == NULL && ptr_limit->rightChild == NULL);
+    while(ptr_limit->headOrder != NULL){
+        deleteOrder(popOrder(ptr_limit));
+    }
+    free(ptr_limit);
 }
 
 Limit*
@@ -218,7 +262,145 @@ getMaximumLimit(Limit *limit){
     return (ptr_maximum);
 }
 
-/*
+
+/**
+ * Lower-lever functions to balance a branch of a Binary Search Tree made up
+ * of Limit structs.
+ */
+
+void
+balanceBranch(Limit *limit) {
+    /**
+     * Balance the nodes of the given branch of Limit structs.
+     *
+     * Asserts that limit has a height of at least 2.
+     */
+    assert(getHeight(limit) < 2);
+
+    int balanceFactor = getBalanceFactor(limit);
+    if(balanceFactor > 1){
+        /*Right is heavier.*/
+        balanceFactor = getBalanceFactor(limit->rightChild);
+        if(balanceFactor < 0){
+            rotateRightLeft(limit);
+        }
+        else if(balanceFactor > 0){
+            rotateRightRight(limit);
+        }
+    }
+    else if(balanceFactor < -1){
+        /*Left is heavier.*/
+        balanceFactor = getBalanceFactor(limit->leftChild);
+        if(balanceFactor < 0){
+            rotateLeftLeft(limit);
+        }
+        else if(balanceFactor > 0){
+            rotateLeftRight(limit);
+        }
+    }
+    else{/*Everything is fine, do nothing*/}
+}
+
+void
+rotateLeftLeft(Limit *limit) {
+    /**
+     * Rotate tree nodes for LL Case.
+     *
+     * Reference:
+     *     https://en.wikipedia.org/wiki/File:Tree_Rebalancing.gif
+     */
+    Limit *child = limit->leftChild;
+    if(limitIsRoot(limit->parent)==1 || limit->limitPrice > limit->parent->limitPrice){
+        limit->parent->rightChild = child;
+    }
+    else{
+        limit->parent->leftChild = child;
+    }
+    child->parent = limit->parent;
+    limit->parent = child;
+    Limit* tmp_ptr = child->rightChild;
+    child->rightChild = limit;
+    limit->leftChild = tmp_ptr;
+    return;
+}
+
+void
+rotateLeftRight(Limit *limit) {
+    /**
+     * Rotate tree nodes for LR Case.
+     *
+     * Reference:
+     *     https://en.wikipedia.org/wiki/File:Tree_Rebalancing.gif
+     */
+    Limit *child = limit->leftChild;
+    Limit *grandChild = limit->leftChild->rightChild;
+    child->parent = grandChild;
+    grandChild->parent = limit;
+
+    Limit* tmp_b_ptr = child->leftChild;
+    Limit* tmp_c_ptr = grandChild->leftChild;
+    Limit* tmp_d_ptr = grandChild->rightChild;
+
+    child->rightChild = grandChild->leftChild;
+    limit->leftChild = grandChild;
+    grandChild->leftChild = child;
+    grandChild->rightChild = tmp_d_ptr;
+    child->leftChild = tmp_b_ptr;
+    child->rightChild = tmp_c_ptr;
+    rotateLeftLeft(limit);
+    return;
+}
+
+void
+rotateRightRight(Limit *limit){
+    /**
+     * Rotate tree nodes for RR Case.
+     *
+     * Reference:
+     *     https://en.wikipedia.org/wiki/File:Tree_Rebalancing.gif
+     */
+    Limit *child = limit->rightChild;
+    if(limitIsRoot(limit->parent)==1 || limit->limitPrice > limit->parent->limitPrice){
+        limit->parent->rightChild = child;
+    }
+    else{
+        limit->parent->leftChild = child;
+    }
+    child->parent = limit->parent;
+    limit->parent = child;
+    Limit* tmp_ptr = child->leftChild;
+    child->leftChild = limit;
+    limit->rightChild = tmp_ptr;
+    return;
+}
+
+void
+rotateRightLeft(Limit *limit){
+    /**
+     * Rotate tree nodes for RL Case.
+     *
+     * Reference:
+     *     https://en.wikipedia.org/wiki/File:Tree_Rebalancing.gif
+     */
+    Limit *child = limit->rightChild;
+    Limit *grandChild = limit->rightChild->leftChild;
+    child->parent = grandChild;
+    grandChild->parent = limit;
+    Limit* tmp_b_ptr = child->rightChild;
+    Limit* tmp_c_ptr = grandChild->rightChild;
+    Limit* tmp_d_ptr = grandChild->leftChild;
+    limit->rightChild = grandChild;
+    grandChild->rightChild = child;
+    grandChild->leftChild = tmp_d_ptr;
+    child->leftChild = tmp_c_ptr;
+    child->rightChild = tmp_b_ptr;
+
+    rotateRightRight(limit);
+    return;
+}
+
+
+/**
  * Lower-level functions to detect states, balance factors and heights of the
  * Limit BST's nodes and their children.
  */
